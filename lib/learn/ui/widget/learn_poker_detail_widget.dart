8ceccd54e17c60fddd828b51ab9c509442cee2ce
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2024-02-09 12:28:15 +0530

    Added Dialog in LearnPokerDetailWidget

--- lib/learn/ui/widget/learn_poker_detail_widget.dart
+++ lib/learn/ui/widget/learn_poker_detail_widget.dart
@@ -14,37 +14,80 @@ class LearnPokerDetailWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          title,
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16.0,
-            color: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        _showDialog(context);
+      },
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+              color: Colors.black,
+            ),
           ),
-        ),
-        const Divider(),
-        Text(
-          description,
-          style: const TextStyle(
-            color: Colors.black,
+          const Divider(),
+          Text(
+            description,
+            style: const TextStyle(
+              color: Colors.black,
+            ),
           ),
-        ),
-        const SizedBox(height: 10),
-        Container(
-          height: 200,
-          color: Colors.grey,
-          child: Image.asset(
-            imageUrl,
-            fit: BoxFit.cover,
+          const SizedBox(height: 10),
+          Container(
+            height: 200,
+            color: Colors.grey,
+            child: Image.asset(
+              imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        const SizedBox(height: 16),
-        const Divider(),
-      ],
+          const SizedBox(height: 16),
+          const Divider(),
+        ],
+      ),
     );
   }
 
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            title,
+            style: const TextStyle(fontSize: 20),
+          ),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 250,
+                child: Image.asset(
+                  imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(description),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                "close",
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
-
